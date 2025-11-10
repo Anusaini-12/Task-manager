a1c4a080bb75461053d4762e29ef036705925690
Anus@ini12 <anusaini1312003@gmail.com>
2025-11-10 14:50:27 +0530

    Initial commit of MERN project

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
