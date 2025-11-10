2b0c4e9700fe47ad7caa6d7a929b699bc3c9dc1e
Anuradha <164632909+Anusaini-12@users.noreply.github.com>
2025-11-10 17:05:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "*",
+    origin: "https://task-manager-mhzj.vercel.app/",
     credentials: true,
 }));
 app.use(express.json());
